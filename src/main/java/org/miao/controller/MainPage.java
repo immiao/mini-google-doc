fbcb3527eb96893540c50ff9fffe4895a36fbcbd
kmiao <miaokaixiang@gmail.com>
2018-01-01 13:45:47 -0500

    Set up basic Spring Boot

---
+++ src/main/java/org/miao/controller/MainPage.java
@@ -0,0 +1,21 @@
+package org.miao.controller;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class MainPage {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MainPage.class, args);
+    }
+}
