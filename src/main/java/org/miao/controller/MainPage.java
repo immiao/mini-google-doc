33c38271374bd5daef33c5bc2dfd60a31938cf06
kmiao <miaokaixiang@gmail.com>
2018-01-11 10:36:09 -0500

    Use JDK 1.8

--- src/main/java/org/miao/controller/MainPage.java
+++ src/main/java/org/miao/controller/MainPage.java
@@ -14,8 +14,4 @@ public class MainPage {
     String home() {
         return "Hello World!";
     }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(MainPage.class, args);
-    }
 }
